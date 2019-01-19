171fdf3437a946bacfd5ebd0c2b66cec2a77ddd7
Andrew Lau <andrew.ondu.lau@gmail.com>
2019-01-20 00:05:50 +1100

    adding GUI file

---
+++ twenty_forty_eight_GUI.py
@@ -0,0 +1,10 @@
+"""
+GUI implementation of twenty forty eight game.
+"""
+
+try:
+    import simplegui
+except ImportError:
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+
+import twenty_forty_eight
